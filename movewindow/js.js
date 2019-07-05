b9755b9b0cf3ef638a9a5e6efba82b563e26e640
pjogh <ivanovp@protonmail.com>
2019-07-05 18:58:09 +0200

    work variant

--- movewindow/js.js
+++ movewindow/js.js
@@ -1,48 +1,47 @@
-let block = document.getElementById('block');
-let header = document.getElementById('header');
+// let block = document.getElementById('block');
+// let header = document.getElementById('header');
 
-header.onmousedown = function(obj) {
-  var coords = getCoords(header);
-  var shiftX = obj.pageX - coords.left;
-  var shiftY = obj.pageY - coords.top;
+// header.onmousedown = function(obj) {
+//   var coords = getCoords(header);
+//   var shiftX = obj.pageX - coords.left;
+//   var shiftY = obj.pageY - coords.top;
 
-  block.style.position = "absolute";
-  document.body.appendChild(block);
-  moveAt(obj);
+//   block.style.position = "absolute";
+//   document.body.appendChild(block);
+//   moveAt(obj);
 
-  block.style.zIndex = 1000;
+//   block.style.zIndex = 1000;
 
-  function moveAt(e) {
-    block.style.left = e.pageX - shiftX + "px";
-    block.style.top = e.pageY - shiftY + "px";
-  }
+//   function moveAt(e) {
+//     block.style.left = e.pageX - shiftX + "px";
+    // block.style.top = e.pageY - shiftY + "px"
 
-  document.onmousemove = function(e) {
-    moveAt(e);
-  };
+//   document.onmousemove = function(e) {
+//     moveAt(e);
+//   };
 
-  block.onmouseup = function() {
-    document.onmousemove = null;
-    block.onmouseup = null;
-  };
-};
+//   block.onmouseup = function() {
+//     document.onmousemove = null;
+//     block.onmouseup = null;
+//   };
+// };
 
-block.ondragstart = function() {
-  return false;
-};
+// block.ondragstart = function() {
+//   return false;
+// };
 
-function getCoords(elem) {
-  var box = elem.getBoundingClientRect();
-  return {
-    top: box.top + pageYOffset,
-    left: box.left + pageXOffset
-  };
-}
+// function getCoords(elem) {
+//   var box = elem.getBoundingClientRect();
+//   return {
+//     top: box.top + pageYOffset,
+//     left: box.left + pageXOffset
+//   };
+// }
 
 function Block (options)
 {
   options = options && typeof(options) === "object" ? options : {};
-  this.target = document.querySelector(options.target);
+  this.target = options.elem;
 
   if (!this.target)
   {
@@ -53,6 +52,37 @@ function Block (options)
 Block.prototype = {
   move: function ()
   {
-    
+    let coords = this.getCoords(event.target);
+    let shiftX = event.pageX - coords.left;
+    let shiftY = event.pageY - coords.top;
+
+    if (!event.target.onmouseup)
+    {
+      
+    }
+    console.log(event.pageX);
+    console.log(coords.left);
+    console.log(shiftX);
+  },
+
+  getCoords: function (elem)
+  {
+    var box = this.target.getBoundingClientRect();
+    return {
+      top: box.top + pageYOffset,
+      left: box.left + pageXOffset
+    };
   },
 }
+
+const block = document.querySelector('#block');
+const header = block.querySelector('#header');
+
+const el = new Block ({
+  elem: block,
+  
+});
+
+header.addEventListener("mousedown", function(){
+  el.move();
+});
